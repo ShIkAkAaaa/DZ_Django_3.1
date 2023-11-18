7c7946f20de1d42d856e363bf33ee4ae3e21db0f
ShIkAkA <sergrudakov89@yandex.ru>
2023-11-18 16:34:42 +0300

    views_2

--- databases/work_with_database/phones/views.py
+++ databases/work_with_database/phones/views.py
@@ -1,13 +1,27 @@
 from django.shortcuts import render
+from .models import Phone
 
 
 def show_catalog(request):
     template = 'catalog.html'
-    context = {}
+    phones = Phone.objects.all()
+    match request.GET.get('sort'):
+        case 'name':
+            phones = phones.order_by('name')
+        case 'max_price':
+            phones = phones.order_by('price')
+        case 'min_price':
+            phones = phones.order_by('-price')
+    context = {
+        'phones': phones,
+    }
     return render(request, template, context)
 
 
 def show_product(request, slug):
     template = 'product.html'
-    context = {}
+    phone = Phone.objects.get(slug=slug)
+    context = {
+        'phone': phone
+    }
     return render(request, template, context)
